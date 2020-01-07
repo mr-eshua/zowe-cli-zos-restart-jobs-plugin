88246f66d072debe4922b0bbaf9939a77ab15255
Roman Kupriyanov <mr.eshua@gmail.com>
2020-01-08 01:22:17 +0300

    Return default Health Check handler (#12)

--- __tests__/imperative.test.ts
+++ __tests__/imperative.test.ts
@@ -5,6 +5,7 @@ describe("imperative config", () => {
     it("config should match expected values", () => {
         const config = require("../src/imperative");
         expect(config.name).toBe("zos-restart-jobs");
+        expect(config.pluginHealthCheck).toContain("healthCheck.Handler");
         expect(config.pluginSummary).toBe("Zowe CLI restart z/OS jobs plug-in");
         expect(config.productDisplayName).toBe("Zowe CLI z/OS Jobs Restart Plug-in");
         expect(config.rootCommandDescription).toBe("Plugin, which allows to restart z/OS jobs");
--- src/imperative.ts
+++ src/imperative.ts
@@ -2,6 +2,7 @@ import { IImperativeConfig } from "@zowe/imperative";
 
 const config: IImperativeConfig = {
     commandModuleGlobs: ["**/cli/*/*.definition!(.d).*s"],
+    pluginHealthCheck: __dirname + "/healthCheck.Handler",
     pluginSummary: "Zowe CLI restart z/OS jobs plug-in",
     rootCommandDescription: "Plugin, which allows to restart z/OS jobs",
     productDisplayName: "Zowe CLI z/OS Jobs Restart Plug-in",
